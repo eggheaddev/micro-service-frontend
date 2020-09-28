e28ef13b04ddfcb532df21ba2d83081beb7c29b3
Gil Rudolf Härdi <ghaerdi@ipisdosaj.edu.do>
2020-09-28 18:59:29 -0400

    some changes on packages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-
+import { Link } from "react-router-dom";
 // Components
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
@@ -20,18 +20,59 @@ function Home() {
         <SearchPackages onChange={handleInput} />
       </div>
 
-      {!packageMatch ? <AppDescription /> : <Packages search={packageMatch} />}
+      {!packageMatch ? <AppDescription /> : <Modules search={packageMatch} />}
       <Footer />
     </Fragment>
   );
 }
 
-function Packages(props) {
-  return <span>{props.search}</span>;
+function Modules (props) {
+  const modules = [
+    {
+      title: "Trex",
+      description: "Ajo",
+      version: "1.0.0",
+    },
+    {
+      title: "Yolo",
+      description: "perro",
+      version: "0.0.5",
+    },
+    {
+      title: "Perro",
+      description: "El pepe",
+      version: "1.0.5"
+    }
+  ];
+
+  return (
+    <div className="packages-container container mb-5">
+      {modules.map(module => (
+        <ModuleCard
+          title={module.title}
+          description={module.description}
+          version={module.version}
+        />
+      ))}
+    </div>
+  );
 }
 
-// function PackageCard(props) {
-//   return <span>package</span>;
-// }
+function ModuleCard(props) {
+  return (
+    <Link
+      to={`/package/${props.title}@${props.version}v`}
+      className="package-link"
+    >
+      <div className="package-card">
+        <div className="package-title">
+          <h1 className="mr-3">{props.title}</h1>
+          <span>{props.version}v</span>
+        </div>
+        <span>{props.description}</span>
+      </div>
+    </Link>
+  );
+}
 
 export default Home;
