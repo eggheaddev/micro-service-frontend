765c78e04fb1df57207eb2817a93f9d2db8df0be
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2020-10-10 00:03:53 -0400

    nav, search, description and modules are now responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,25 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <Fragment>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="nav-container">
-          <Link to="/" className="navbar-brand">
-            <h1 className="logo">Hostify</h1>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to="/documentation" className="nav-link">
-                  Documentation
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/login">
-                  <button type="button" className="btn sign-up-btn ml-4">
-                    Get a token
-                  </button>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </Fragment>
+    <nav className="nav-container">
+      <Link to="/">
+        <h1 className="logo">Hostify</h1>
+      </Link>
+        <ul>
+          <li className="nav-item">
+            <Link to="/documentation">Documentation</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login">
+              <button type="button" className="btn sign-up-btn">
+                Get a token
+              </button>
+            </Link>
+          </li>
+        </ul>
+    </nav>
   );
 }
 
